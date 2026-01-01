591ae58ebe0c1b1f59257c2657f6f4f36694bf12
Nishant-Kr-Shaw <shawnishantkr1@gmail.com>
2026-01-01 13:12:26 +0530

    Changes in Config file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+    },
+  },
 })
+
+
